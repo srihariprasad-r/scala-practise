885265ad25e3af8a7d5bca6d013088adfa930e77
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-03-18 00:16:56 +0530

    updated vector class

---
+++ src/oodetails/MutableVect2D.scala
@@ -0,0 +1,38 @@
+package oodetails
+
+class MutableVect2D(private var _x: Double, private var _y: Double) {
+  def x = _x
+  
+  def y = _y
+  
+  def setX(newX: Double) :Unit = _x = newX
+  
+  def setY(newY: Double) :Unit = _y = newY  
+  
+  def plus(mv:MutableVect2D):MutableVect2D = {
+    _x += mv.x
+    _y += mv.y
+    this
+  }
+  
+  def minus(mv:MutableVect2D):MutableVect2D = {
+    _x -= mv.x
+    _y -= mv.y
+    this
+  }
+  
+  def scale(c:Double):MutableVect2D = {
+    _x *= c
+    _y *= c
+    this
+  }  
+}
+
+object MutableVect2D {        //companion object
+  def main(args:Array[String]):Unit = {
+    val v1 = new MutableVect2D(1,2)
+    val v2 = new MutableVect2D(2,2)
+    v1.plus(v2)
+    println(v1)
+  }
+}
