5b86ddd3379c591386885a63b48776ee63ba5e74
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-03-21 01:16:47 +0530

    added visibility settings

--- src/polymorphism/MutableSquare.scala
+++ src/polymorphism/MutableSquare.scala
@@ -1,3 +1,5 @@
 package polymorphism
 
-class MutableSquare(private var _length: Double) extends MutableRectangle(_length, _length)
+class MutableSquare(_length: Double) extends MutableRectangle(_length, _length) {
+  //def length = _width      //_width is not seen as it is private in parent class, but is visible when protected
+}
