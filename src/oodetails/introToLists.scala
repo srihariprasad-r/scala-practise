c3924bf488b458a6224e0abbe83a2988d9fa8493
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-03-23 00:43:51 +0530

    list functions collections as working copy

--- src/oodetails/introToLists.scala
+++ src/oodetails/introToLists.scala
@@ -6,8 +6,14 @@ object introToLists {
     val list1 = List(2)
     val list2 = List.fill(6)(0)
     val list3 = List.tabulate(5)(x=> x*2)
-    println(list3.init) //List(0, 2, 4, 6)
-    println(list3.head) // 0
-    println(list3.tail) //List(2, 4, 6, 8)
+    val list4 = list2 ++ list3 
+    val list5 = 3 +: list2  
+    val list6 = list2 :+ 6
+    //println(list6)      //List(0, 0, 0, 0, 0, 0, 6)
+    //println(list5)    //same as :: List(3, 0, 0, 0, 0, 0, 0)
+    //println(list4)    //same as ::: List(0, 0, 0, 0, 0, 0, 0, 2, 4, 6, 8)
+    //println(list3.init) //List(0, 2, 4, 6)
+    //println(list3.head) // 0
+    //println(list3.tail) //List(2, 4, 6, 8)
   }
 }
