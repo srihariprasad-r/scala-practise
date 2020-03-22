ecd59b2373cd87a1db4827fd31e280d28d82f52f
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-03-23 00:51:45 +0530

    list functions collections folding functions

--- src/oodetails/introToLists.scala
+++ src/oodetails/introToLists.scala
@@ -4,11 +4,26 @@ object introToLists {
   def main(args:Array[String]):Unit = {
     val list0 = List() //creates an empty List
     val list1 = List(2)
+    
+    //auto fill elements with default values based on size
     val list2 = List.fill(6)(0)
+    
+    //fill values on given based on function literal
     val list3 = List.tabulate(5)(x=> x*2)
-    val list4 = list2 ++ list3 
+    val list4 = list2 ++ list3
+    
+    //append elements before & after 
     val list5 = 3 +: list2  
     val list6 = list2 :+ 6
+    
+    //foldleft
+    val list7 = (0 /: list3)((acc, elem) => (acc+elem))
+    
+    //foldright
+    val list8 = (list3 :\ 410)((acc,elem) => (acc+elem))
+    
+    println(list8)      //accumulator starts from 410 and moves right to left
+    //println(list7)    //accumulator starts from 0 and adds element one by one
     //println(list6)      //List(0, 0, 0, 0, 0, 0, 6)
     //println(list5)    //same as :: List(3, 0, 0, 0, 0, 0, 0)
     //println(list4)    //same as ::: List(0, 0, 0, 0, 0, 0, 0, 2, 4, 6, 8)
