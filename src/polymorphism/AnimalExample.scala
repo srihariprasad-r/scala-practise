1bedafd73e26b53a651f50ce65e2ecd3c0fd06c5
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-04-01 01:24:44 +0530

    added implicit variables/function examples

--- src/polymorphism/AnimalExample.scala
+++ src/polymorphism/AnimalExample.scala
@@ -1,4 +1,5 @@
 package polymorphism
+import scala.language.implicitConversions
 
 class AnimalExample(color: String) {
   val animalColor = color
@@ -17,7 +18,7 @@ abstract class Animal(color:String) {
 
 //tried to extend 2 classes, but throws error
 //traits can be mixed with class using with keyword
-class Cat(val color: String, tricks:List[String]) extends AnimalExample(color) with Tricks //extends Tricks(trick)
+class Cat(val color: String, val tricks:List[String]) extends AnimalExample(color) with Tricks //extends Tricks(trick)
 {
   override val animalColor = color 
   def trickName = println(tricks)
@@ -26,6 +27,8 @@ class Cat(val color: String, tricks:List[String]) extends AnimalExample(color) w
 class Dog(val color: String) extends AnimalExample(color) {
   override def sound =  {println("I bark")} 
   def newMethod = { println("Hi")}
+  def food(morning: String)(implicit evening: String) = { println("I have " + morning + " and: " + evening)}
+  def enoughFood(implicit quantity: String) = println("I had "+ quantity + " cups!")
 }
 
 class myPet(a: AnimalExample){
@@ -35,13 +38,21 @@ class myPet(a: AnimalExample){
 object AnimalExample{
   def main(args: Array[String]): Unit = {
     //val an = new Animal("black")    //this throws error as class is abstract
-    
+ 
+    implicit val animalFood : String = "Fasting all evening!" 
+    implicit def foodQuantity (a: Int) = a.toString 
     val c = new Cat("blue", List("jump", "sit"))
     println(c.animalColor)  //prints blue
     println(c.trickName)  //List(jump, sit)
     val d = new Dog("black")
     println(d.animalColor)  //prints black
     println(d.sound)   //prints "I bark"
+    
+    //below proves implicit conversion
+    println(d.food("Pedigree")) //prints "I have Pedigree and: Fasting all evening!" 
+    
+    //below takes integer as input and implicitly converts to string
+    println(d.enoughFood(20))  //prints "I had 20 cups!"
     
     val a = new myPet(d)
     println(a.description)   //prints "My pet is polymorphism.Dog@5f184fc6 and it has color: black"
