4bd6d0643f89e84aea8f8c30dadafa1a9b76f25f
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-03-31 23:24:18 +0530

    added abstract/traits examples

--- src/polymorphism/AnimalExample.scala
+++ src/polymorphism/AnimalExample.scala
@@ -5,12 +5,27 @@ class AnimalExample(color: String) {
   def sound = { println("No sound") }
 }
 
-class Cat(val color: String) extends AnimalExample(color) {
+trait Tricks {
+  def trickName
+}
+
+//use abstract class when there is no need of instantiation of objects 
+abstract class Animal(color:String) {
+  def sound:Unit
+  val animalComor = color
+}
+
+//tried to extend 2 classes, but throws error
+//traits can be mixed with class using with keyword
+class Cat(val color: String, tricks:List[String]) extends AnimalExample(color) with Tricks //extends Tricks(trick)
+{
   override val animalColor = color 
+  def trickName = println(tricks)
 }
 
 class Dog(val color: String) extends AnimalExample(color) {
   override def sound =  {println("I bark")} 
+  def newMethod = { println("Hi")}
 }
 
 class myPet(a: AnimalExample){
@@ -19,6 +34,11 @@ class myPet(a: AnimalExample){
 
 object AnimalExample{
   def main(args: Array[String]): Unit = {
+    //val an = new Animal("black")    //this throws error as class is abstract
+    
+    val c = new Cat("blue", List("jump", "sit"))
+    println(c.animalColor)  //prints blue
+    println(c.trickName)  //List(jump, sit)
     val d = new Dog("black")
     println(d.animalColor)  //prints black
     println(d.sound)   //prints "I bark"
