4e238b7c6fb68ec5913901bef628a7302de2823a
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-04-08 01:02:22 +0530

    included generic disjoint types using case classes

--- src/oodetails/introFunctions.scala
+++ src/oodetails/introFunctions.scala
@@ -2,7 +2,9 @@ package oodetails
 
 object introFunctions {
   def main(args: Array[String]):Unit = {
-
+    
+    /************************************************************************************************/
+    
     //Higher order functions
     def loggerFunc(x: String): (String => Unit) = {
       t => println(s"$x returned $t")
@@ -15,18 +17,39 @@ object introFunctions {
     //below code as works!
     //println(loggerFunc("logger completed")("Success"))
     
-    //Disjoint Type - Either[A, B]
+    /************************************************************************************************/
+    
+    //Disjunction Type - Either[A, B]
     //above function can be written using Either[String, Int] which either prints error or returns result
     def disjointLoggerFunc(x: Either[String, Int]): Int = x match {
       case Left(a) => println(s"Error raised: $a"); -1      //this expression prints a message and return Int (-1)
       case Right(res) => res
     }
     
-    println(disjointLoggerFunc(Left("Logger failure"))) //prints "Error raised: Logger failure  -1"
+    //println(disjointLoggerFunc(Left("Logger failure"))) //prints "Error raised: Logger failure  -1"
     
     //below function call will execute Right(..) case expression
-    println(disjointLoggerFunc(Right(32)))    //prints 32
+    //println(disjointLoggerFunc(Right(32)))    //prints 32
+    
+    //To create more general disjunct types, we will mix in traits and case classes
+    
+    sealed trait MyType
+    final case class inputAsList(a: List[Int]) extends MyType
+    final case class inputAsInt(a: Int) extends MyType
+    final case class inputAsBoolean(a: Int, x: Boolean) extends MyType    
+    
+    def genericDisjoint(x: MyType) = x match {
+      case inputAsList(z) => z.head
+      case inputAsInt(m) => m*10
+      case inputAsBoolean(n, b) => !b
+    }
+    
+    //below will execute the function which are generic on Types
+    println(genericDisjoint(inputAsList(List(1,2,3))))    //prints 1
+    println(genericDisjoint(inputAsInt(30)))              //prints 300
+    println(genericDisjoint(inputAsBoolean(30, true)))    //prints false   
    
   }
 }
 
+ 
