09eec76815ed725a4338c5ecc256850b08472674
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-04-16 00:51:54 +0530

    added linked list insert/update functions

--- src/Datastructures/linkedlist.scala
+++ src/Datastructures/linkedlist.scala
@@ -10,10 +10,31 @@ class linkedlist[A] {
   
   //apply method is a companion object which would move the pointer to needed loction
   def apply(index: Int): A  = {
+    require(index >= 0 )
     //initial position of cursor is set to head
     var cursor = head
     for (i <- 0 until index) cursor = cursor.next
     cursor.data
+  }
+  
+  def update(index: Int, data: A) {
+    require(index >= 0 )
+    //initial position of cursor is set to head
+    var cursor = head
+    for (i <- 0 until index) cursor = cursor.next
+    cursor.data = data    
+  }
+  
+  def insert(index: Int, data: A) {
+    require(index >= 0 )
+    if (index == 0) {
+      head = new Node(data, head)
+    } else {
+    //initial position of cursor is set to head
+    var cursor = head
+    for (i <- 0 until index-1) cursor = cursor.next
+    cursor.next = new Node(data, cursor.next)
+  }
   }
   
 }
