fe2f12e4491bc92836136bdd9023fe80c6340995
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-04-17 01:04:40 +0530

    added method for append/prepend on buffer list

--- src/Datastructures/bufferSinglylinkedlist.scala
+++ src/Datastructures/bufferSinglylinkedlist.scala
@@ -8,8 +8,8 @@ abstract class bufferSinglylinkedlist[A] extends mutable.Buffer[A]{
   private class Node(var data: A, var next: Node)
   
   private var fst: Node = null
-  private var lst: Node = null
-  private var count: Int = 0
+  private var lst: Node = null        //added lst node as tail node to help in reducing the O(n) traversal
+  private var count: Int = 0          //added this count to perform O(1) for call on length
   
   def length: Int = count
   
@@ -18,6 +18,28 @@ abstract class bufferSinglylinkedlist[A] extends mutable.Buffer[A]{
     var lst = null
     var count = 0
   }
+  
+  //below method will prepend elements to existing linked list
+  def +=:(elem: A) = {
+    fst = new Node(elem, fst)
+    if (lst == null) lst = fst        //this check is performed if linked list is empty and if so, head and tail will point to same node
+    count += 1                       // increment count by 1 after prepend
+    this
+  }
+  
+  //below method will append elements to existing linked list
+  def +=(elem: A) = {
+    if (lst == null) {              //check if list is empty, if so, add new node and have head and tail point to same node
+      fst = new Node(elem, null)
+      lst = fst
+    } else {
+      lst.next = new Node(elem, null)      //create a new node which is last element
+      lst = lst.next                       //move the tail to new node created above
+    }
+    count += 1                            //increment count by 1 after append
+    this
+  }
+  
   
   def apply(index: Int): A = {
     require(index >= 0 && index < count)
