23349f511250945064d042b649b593d344bbffd7
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-04-19 00:45:05 +0530

    added push implementation of array stack class

--- src/Datastructures/ArrayStack.scala
+++ src/Datastructures/ArrayStack.scala
@@ -7,7 +7,15 @@ class ArrayStack[A: ClassTag] {
   private var data = new Array[A](10)
   private var index = 0
   
-  def push(a: A) : Unit = ???
+  def push(a: A) : Unit = {
+    if (index >= data.length){
+      val tmp = new Array[A](data.length * 2)      //chose 2 as it is amortized cost which is on average results in O(1)
+      Array.copy(data, 0, tmp, 0, data.length)
+      data = tmp      
+    }
+    data(index) = a            
+    index += 1
+  }
   
   def pop(): A = {
     index -= 1
