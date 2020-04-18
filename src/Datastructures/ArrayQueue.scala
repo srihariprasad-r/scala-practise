8d022c0302b61f72114982a60bd7aa50076e5849
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-04-19 01:10:18 +0530

    added arrayqueue class

---
+++ src/Datastructures/ArrayQueue.scala
@@ -0,0 +1,32 @@
+package Datastructures
+
+import scala.reflect.ClassTag
+
+class ArrayQueue[A: ClassTag] {
+
+  private var data = new Array[A](10)
+  private var popindex = 0 
+  private var pushindex = 0
+  
+  def enqueue(a: A): Unit = {
+    if ((pushindex + 1) % data.length == popindex) {
+      val tmp = new Array[A](data.length * 2)
+      for (i <- 0 until data.length - 1) tmp(i) = data((popindex +1) % data.length)
+      data = tmp
+      popindex = 0
+      pushindex = data.length - 1
+    }
+  }
+  
+  def dequeue(): A = {
+    val ret = data(popindex)
+    popindex = (popindex + 1) % data.length
+    ret
+  }
+  
+  def peek(): A = data(popindex)
+  
+  def isEmpty: Boolean = popindex == pushindex      
+  
+  
+}
