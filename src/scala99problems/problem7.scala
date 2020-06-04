2ca9250b57c068d7fcc6e5109df45eda840a9c2f
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-06-05 00:20:31 +0530

    added problem9 for 99 scala problems

--- src/scala99problems/problem7.scala
+++ src/scala99problems/problem7.scala
@@ -4,8 +4,8 @@ class problem7 {
     //this method is needed as flattem cannot be applied when there are primitive data types such as Int
     def flattenList[A](l:List[Any]):List[Any] = l match {
     case Nil => Nil     
-    case (l: List[_]) :: tail => flattenList(l):::flattenList(tail)    
-    case h::tail => h::flattenList(tail)
+    case (l: List[_]) :: tail => flattenList(l):::flattenList(tail)
+    case h::tail => h::flattenList(tail)    
   }
 }
 
@@ -13,6 +13,11 @@ object problem7 {
   def main(args: Array[String]):Unit = {
     val p7 = new problem7
     val list1 = List(List(1, 1), 2, List(3, List(5, 8)))
-    println(p7.flattenList(list1))    //List(1, 1, 2, 3, 5, 8)
+    //println(p7.flattenList(list1))    //List(1, 1, 2, 3, 5, 8)
+    } 
+    
   }
-}
+
+
+
+
