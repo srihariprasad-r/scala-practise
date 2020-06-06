9257b994a2c6ec983496458394bdd7e477bf3f83
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-06-06 23:44:25 +0530

    new version with span method for permutation class

--- src/miscellaneous/permutations.scala
+++ src/miscellaneous/permutations.scala
@@ -13,14 +13,25 @@ class permutations {
       }
     }
   }
+  
+ //permutation using span method on List iterable 
+ def perms[A](xs: List[A]) : List[List[A]] = xs match {
+   case Nil => List(xs)
+   case _ => for {
+           x <- xs
+           ; val (l,r) = xs span { x!= _ }
+           ; ys <- perms(l ++ r.tail)    
+       } yield (x :: ys)
+  }
+
 }
 
 object permutations {
   def main(args:Array[String]):Unit = {
   val p1 = new permutations
-  println(p1.permutations(List(1, 2, 3)))
+  //println(p1.permutations(List(1, 2, 3)))
   //println(List(100,200,300).take(1) ++ List(100,200,300).drop(2))
-  
+  println(p1.perms(List(1,2,3)))
 }
 } 
 
