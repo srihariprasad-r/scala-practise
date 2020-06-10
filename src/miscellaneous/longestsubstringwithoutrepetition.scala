9b86f6c7719fbfabdb56ae5da5835eddd59514b8
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-06-11 00:50:29 +0530

    correct version of longest non repeating substring

--- src/miscellaneous/longestsubstringwithoutrepetition.scala
+++ src/miscellaneous/longestsubstringwithoutrepetition.scala
@@ -5,20 +5,27 @@ import scala.collection.mutable
 class longestsubstringwithoutrepetition {
   def func(s: String): Int = {
     
-    var map = mutable.ArrayBuffer[Char]()
+    var map = mutable.HashSet[Char]()
+    var j = 0 
+    var i = 0
+    
+    var max = 0 
     
     s.indices.foreach(idx =>
       idx match {
-        case idx if (map.isEmpty) => map = mutable.ArrayBuffer(s(idx))
-        case idx if ((!map.isEmpty) && map.contains(s(idx)) == false ) => map += s(idx)  
-        case idx if ((!map.isEmpty) && map.contains(s(idx)) == true ) => {
-          map -= s(idx)
-          map += s(idx)
+        case idx if (map.isEmpty == true) => map.add(s(idx))
+        case idx if ((!map.isEmpty) && map.contains(s(idx)) == false ) => {
+          map.add(s(idx))
+          j += 1
+          max = math.max(max, map.size)
+        }
+        case idx if ((!map.isEmpty) && map.contains(s(i)) == true ) =>  {
+          map.remove(s(i))
+          i += 1
         }
       }
    )
-   
-   map.length
+   max
           
   }
 }
@@ -27,7 +34,7 @@ object longestsubstringwithoutrepetition {
   def main(args: Array[String]): Unit = {
     val lst = new longestsubstringwithoutrepetition
     
-    val s = "abcabcbb"
+    val s = "pwwkew"
     println(lst.func(s))
   }
 }
