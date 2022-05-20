8075771340bef1d9d9099ab9d88610e9702aca32
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-05-20 22:08:36 +0530

    update to type systems

--- src/rtj/TypeLevelProgramming.scala
+++ src/rtj/TypeLevelProgramming.scala
@@ -1,23 +1,25 @@
 package rtj
 
+//tried on https://scastie.scala-lang.org/
 object TypeLevelProgramming {
-  
+
   trait Nat
   class zero extends Nat
   class Succ[M <: Nat] extends Nat
-  
+
   type one = Succ[zero]
   type two = Succ[one]
   type three = Succ[two]
-  
-  trait lessThan[A <: Nat, B <: Nat]
-  object < extends lessThan {
-    //implicit def validate[N <: Nat]: lessThan[zero, Succ[N]] = ???
-    //def inductive[A <: Nat, B <: Nat]: lessThan[Succ[A], Succ[B]]  = ???
-    def apply[A <: Nat, B <: Nat]: lessThan[A, B] = lessThan[A, B]
-    
+
+  trait <[A <: Nat, B <: Nat]
+  object < {
+    //given basic with type N, creates instance of <[zero, Succ[N]] with implementation in {}
+    given validate[N <: Nat]: <[zero, Succ[N]] with {}
+    given inductive[A <: Nat, B <: Nat](using <[A, B]): <[Succ[A], Succ[B]] with {}
+    def apply[A <: Nat, B <: Nat](using lt: <[A, B]): <[A, B] = lt
+
     val zerolessthantwo = <[zero, two]
-    val threelessthanOne = <[three, one] 
-    
+    //no implicit argument of type TypeLevelProgramming.three < TypeLevelProgramming.one was found for parameter lt of method apply in object <.:
+    val threelessthanOne = <[three, one]
   }
 }
