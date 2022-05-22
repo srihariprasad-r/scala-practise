3f6057b172d45b96a772090ff22bce30207e6f46
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-05-22 22:16:35 +0530

    companion object example update

--- scala3/src/main/scala/rtj/Person.scala
+++ scala3/src/main/scala/rtj/Person.scala
@@ -2,18 +2,26 @@ package rtj
 
 class Person(val name: String) {
   import Person._
-  def name(fname: Person): Person = ???
+  //import scala.runtime.ScalaRunTime.stringOf
+  def parent(): List[String] = {
+    val s = this.name
+    s.split(" ").toList
+  }
 }
 
-object Person extends App{
+object Person{
   def calculateAge(n: Int): Int = n
   def isMale(gender: Boolean): Boolean = gender
-  def apply(name: String) = new Person(name)
+  def apply(father: Person, mother: Person) = {
+    val child = s"${father.name} ${mother.name}"
+    new Person(child)
+  }
 }
 
 def main(args: Array[String]): Unit = {
-  val fp1 = Person("FP1")
-  //val mp1 = Person("MP1")
-  //println(Person.parent(fp1, mp1))
+  val fp1 = new Person("FP1")
+  val mp1 = new Person("MP1")
+  val p1 = Person(fp1, mp1)
+  println(p1.parent()) //Array(FP1, MP1)
   println(Person.calculateAge(10))
 }
