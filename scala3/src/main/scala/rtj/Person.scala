c9bd6f13fffbbd06ab5c195d66af44ca8a46dc35
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-05-23 21:52:14 +0530

    removed companion object to include inheritance

--- scala3/src/main/scala/rtj/Person.scala
+++ scala3/src/main/scala/rtj/Person.scala
@@ -1,32 +1,29 @@
 package rtj
 
 class Person(val name: String) {
-  //import Person._
   //import scala.runtime.ScalaRunTime.stringOf
-}
-
-class Parent(val father: Person = null, val mother: Person = null){
-  import Person._
-  def parent(): Boolean = this match {
-    case father if mother == null => Person.isMale(true)
-    case mother if father == null => Person.isMale(false)
-    case _ if father != null && mother != null => Person.isMale(true)
-    case null => false
-  }
-  def getParentName(p: Person): String = Person.parentName(p)
-}
-
-object Person extends App{
   def calculateAge(n: Int): Int = n
   def isMale(gender: Boolean): Boolean = gender
   def parentName(p: Person): String = p.name
 }
 
+class Parent(val father: Person = null, val mother: Person = null) extends Person("") {
+  def parent(): Boolean = this match {
+    case father if mother == null => isMale(true)
+    case mother if father == null => isMale(false)
+    case _ if father != null && mother != null => isMale(true)
+    case null => false
+  }
+  def getParentName(p: Person): String = parentName(p)
+}
+
+object Person {}
+
 def main(args: Array[String]): Unit = {
   val fp1 = new Person("FP1")
   val mp1 = new Person("MP1")
   val p1 = new Parent(fp1, mp1)
-  println(p1.parent()) //Array(FP1, MP1)
+  println(p1.parent())
   println(p1.getParentName(mp1))
-  println(Person.calculateAge(10))
+  println(p1.calculateAge(10))
 }
