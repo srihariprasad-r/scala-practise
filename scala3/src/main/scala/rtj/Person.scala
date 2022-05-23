7b2d175d0d19792d299f4d2d6ba218204ba0780a
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-05-23 20:45:45 +0530

    added case match for constructor

--- scala3/src/main/scala/rtj/Person.scala
+++ scala3/src/main/scala/rtj/Person.scala
@@ -1,30 +1,37 @@
 package rtj
 
+class Parent(val father: Person = null, val mother: Person = null) {
+  import Person._
+  def parent(): Boolean = this match {
+    case father if mother == null => Person.isMale(true)
+    case mother if father == null => Person.isMale(false)
+    case _ if father != null && mother != null => Person.isMale(true)
+    case null => false
+  }
+}
+
 class Person(val name: String) {
   import Person._
   //import scala.runtime.ScalaRunTime.stringOf
-  def parent(): String = {
+  def parent(): Boolean = {
     val s = this.name
     s.split(" ").toList match {
-      case x :: y :: Nil => s"Father: ${x} Mother: ${y}"
-      case _ => "None"
+      case x :: y :: Nil =>  isMale(true)
+      case _ => false
     }
   }
 }
 
+
 object Person extends App{
   def calculateAge(n: Int): Int = n
   def isMale(gender: Boolean): Boolean = gender
-  def apply(father: Person, mother: Person) = {
-    val child = s"${father.name} ${mother.name}"
-    new Person(child)
-  }
 }
 
 def main(args: Array[String]): Unit = {
   val fp1 = new Person("FP1")
   val mp1 = new Person("MP1")
-  val p1 = Person(fp1, mp1)
+  val p1 = new Parent(fp1, mp1)
   println(p1.parent()) //Array(FP1, MP1)
   println(Person.calculateAge(10))
 }
