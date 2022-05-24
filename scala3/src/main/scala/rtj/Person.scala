aae5088dddcdc6f68c0e1d16beec05b761249efd
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-05-24 20:10:11 +0530

    added extractor for case match

--- scala3/src/main/scala/rtj/Person.scala
+++ scala3/src/main/scala/rtj/Person.scala
@@ -8,23 +8,24 @@ class Person(val name: String) {
 }
 
 class Parent(val father: Person = null, val mother: Person = null) extends Person("") {
-  //auxillary constructor
   def this(father: Person) = this(father, null)
+  //unapply case match
   def parent(): Boolean = this match {
-    case father if mother == null => isMale(true)
-    case mother if father == null => isMale(false)
-    case _ if father != null && mother != null => isMale(true)
-    case null => false
+    case Parent(father, null) => isMale(true)
+    case Parent(null, mother) => isMale(false)
+    case Parent(father, mother) => isMale(true)
   }
   def getParentName(p: Person): String = parentName(p)
 }
 
-object Person extends App{}
+object Parent{
+  def unapply(p: Parent): Option[(Person, Person)] = Some(p.father, p.mother)
+}
 
 def main(args: Array[String]): Unit = {
   val fp1 = new Person("FP1")
   val mp1 = new Person("MP1")
-  val p1 = new Parent(father=fp1)
+  val p1 = new Parent(fp1, mp1)
   println(p1.parent())
   println(p1.getParentName(mp1))
   println(p1.calculateAge(10))
